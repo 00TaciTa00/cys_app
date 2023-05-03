de432f429fdd07196f66db4e4e5f11d84859a037
SoSoShSu <tiffany720@naver.com>
2023-05-03 11:26:04 +0900

    camera add commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,233 +1,63 @@
-
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+late List<CameraDescription> _cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  _cameras = await availableCameras();
+  runApp(const CameraApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/// CameraApp is the Main Application.
+class CameraApp extends StatefulWidget {
+  /// Default Constructor
+  const CameraApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<CameraApp> createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(_cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
     return MaterialApp(
-      title: 'CSY Demo',
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade100,
-        body: Column(
-          children: [
-            // Status Bar
-            SizedBox(
-              height: 52,
-              child:Container(
-                color: Colors.black54,
-              ),
-            ),
-            // Camera Area
-            SizedBox(
-              height: 160, 
-               // upper box with menu icon
-              child: Container(
-                height: 160,
-                color: Colors.grey,
-                // Camera Icon
-              ),
-            ),
-            // Body
-            Expanded(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  children: [
-                    // Recent Meal
-                    const SizedBox(
-                      width: 120,
-                      child: Text(
-                        '최근 식단',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          fontSize: 24,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(2.5),
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.rectangle
-                      ),
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            // meal 
-                            Container(
-                              padding: const EdgeInsets.all(2.5),
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                              ),
-                              child: Container(
-                                height: 128,
-                                width: 128,
-                                decoration: const BoxDecoration(
-                                  color: Colors.yellow,
-                                  shape: BoxShape.rectangle
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(2.5),
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                              ),
-                              child: Container(
-                                height: 128,
-                                width: 128,
-                                decoration: const BoxDecoration(
-                                  color: Colors.yellow,
-                                  shape: BoxShape.rectangle
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(2.5),
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                              ),
-                              child: Container(
-                                height: 128,
-                                width: 128,
-                                decoration: const BoxDecoration(
-                                  color: Colors.yellow,
-                                  shape: BoxShape.rectangle
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(2.5),
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                              ),
-                              child: Container(
-                                height: 128,
-                                width: 128,
-                                decoration: const BoxDecoration(
-                                  color: Colors.yellow,
-                                  shape: BoxShape.rectangle
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    // Recommend Meal
-                    const SizedBox(
-                      width: 120,
-                      child: Text(
-                        '추천 식단',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          fontSize: 24,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(2.5),
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.rectangle
-                      ),
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            // meal 
-                            Container(
-                              padding: const EdgeInsets.all(2.5),
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                              ),
-                              child: Container(
-                                height: 128,
-                                width: 128,
-                                decoration: const BoxDecoration(
-                                  color: Colors.yellow,
-                                  shape: BoxShape.rectangle
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(2.5),
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                              ),
-                              child: Container(
-                                height: 128,
-                                width: 128,
-                                decoration: const BoxDecoration(
-                                  color: Colors.yellow,
-                                  shape: BoxShape.rectangle
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(2.5),
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                              ),
-                              child: Container(
-                                height: 128,
-                                width: 128,
-                                decoration: const BoxDecoration(
-                                  color: Colors.yellow,
-                                  shape: BoxShape.rectangle
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(2.5),
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                              ),
-                              child: Container(
-                                height: 128,
-                                width: 128,
-                                decoration: const BoxDecoration(
-                                  color: Colors.yellow,
-                                  shape: BoxShape.rectangle
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            // Bottom
-            SizedBox(
-              height: 64,
-              child: Container(
-                decoration: const BoxDecoration(
-                  border: Border(
-                    top: BorderSide(
-                      width: 1.0, 
-                      color: Colors.grey
-                      ),
-                  ),
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: CameraPreview(controller),
     );
   }
 }
